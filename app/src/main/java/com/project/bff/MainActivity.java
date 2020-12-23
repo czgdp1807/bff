5cbfab58e10f40991e7388bba25fb18ef92ea18d
czgdp1807 <gdp.1807@gmail.com>
2020-12-23 19:47:40 +0530

    YouTubeDataFetcher implemented
    Ignored API Key Files

--- app/src/main/java/com/project/bff/MainActivity.java
+++ app/src/main/java/com/project/bff/MainActivity.java
@@ -10,13 +10,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.project.dataapis.YouTubeDataFetcher;
+
+import java.io.IOException;
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import java.util.TimeZone;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,24 +54,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void sendMessage(View v) {
+    private void addMessageToList(String message, int viewType)
+    {
+        TimeZone timeZone = TimeZone.getDefault();
+        Calendar calendar = Calendar.getInstance(timeZone);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFromat);
+        String data_time = simpleDateFormat.format(calendar.getTime()).toString();
+        MessageChatModel model = new MessageChatModel(
+                message,
+                data_time,
+                viewType
+        );
+        messageChatModelList.add(model);
+        recyclerView.smoothScrollToPosition(messageChatModelList.size());
+        adapter.notifyDataSetChanged();
+    }
+
+    public void sendMessage(View v) throws InterruptedException, ExecutionException {
 
         String msg = messageET.getText().toString();
-        if( msg.length() > 0 ) {
-            TimeZone timeZone = TimeZone.getDefault();
-            Calendar calendar = Calendar.getInstance(timeZone);
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFromat);
-            String data_time = simpleDateFormat.format(calendar.getTime()).toString();
-            MessageChatModel model = new MessageChatModel(
-                    msg,
-                    data_time,
-                    0
-            );
-            messageChatModelList.add(model);
-            recyclerView.smoothScrollToPosition(messageChatModelList.size());
-            adapter.notifyDataSetChanged();
+        if( msg.length() > 0 )
+        {
+            addMessageToList(msg, 0);
             messageET.setText("");
+            generateMessageResponse(msg);
         }
+    }
 
+    private void generateMessageResponse(String msg) throws ExecutionException, InterruptedException {
+        YouTubeDataFetcher youTubeDataFetcher = new YouTubeDataFetcher();
+        youTubeDataFetcher.setAPIKey();
+        HashMap<String, String> query = new HashMap<String, String>();
+        query.put("part", "snippet");
+        query.put("q", "brave");
+        query.put("type", "video");
+        Integer status = youTubeDataFetcher.execute(query).get();
+        HashMap<String, String> msgInfo = youTubeDataFetcher.toHashMap();
+        String responseMsg = "Checkout this video,\nTitle: " +
+                             msgInfo.get("title") + "\nLink: " +
+                             msgInfo.get("url");
+        addMessageToList(responseMsg, 1);
     }
 }
