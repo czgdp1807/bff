43cdb35fcdcfad3908667b8303844f6768ef87f8
czgdp1807 <gdp.1807@gmail.com>
2021-01-17 19:37:40 +0530

    WIP

--- app/src/main/java/com/project/ondevicebot/OnDeviceBotCommon.java
+++ app/src/main/java/com/project/ondevicebot/OnDeviceBotCommon.java
@@ -4,21 +4,35 @@ import android.content.res.AssetFileDescriptor;
 
 import com.project.bff.MainActivity;
 
+import org.tensorflow.lite.Interpreter;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class OnDeviceBotCommon
 {
 
-    protected String ioException;
+    public static String ioException;
 
-    protected MappedByteBuffer loadModelFile(MainActivity mainActivity)
+    public static Interpreter createModelInterpreter(String modelPath, MainActivity mainActivity)
+    {
+        MappedByteBuffer model = loadModelFile(modelPath, mainActivity);
+        if( model != null )
+        {
+            return new Interpreter(model);
+        }
+        return null;
+    }
+
+    protected static MappedByteBuffer loadModelFile(String modelPath, MainActivity mainActivity)
     {
         try
         {
-            AssetFileDescriptor fileDescriptor = mainActivity.getAssets().openFd(this.getModelPath());
+            AssetFileDescriptor fileDescriptor = mainActivity.getAssets().openFd(modelPath);
             FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
             FileChannel fileChannel = inputStream.getChannel();
             long startOffset = fileDescriptor.getStartOffset();
@@ -32,8 +46,16 @@ public class OnDeviceBotCommon
         }
     }
 
-    protected String getModelPath()
+    public static String generateResponse
+    (String msg, HashMap<Integer, String> idx2word,
+     HashMap<String, ArrayList<Double>> word2vec, Interpreter tflite)
     {
+
         return new String("");
+    }
+
+    protected HashMap<Integer, String> readIdx2Word()
+    {
+        return null;
     }
 }
--- app/src/main/java/com/project/ondevicebot/OnDeviceBotLength1_5.java
+++ app/src/main/java/com/project/ondevicebot/OnDeviceBotLength1_5.java
@@ -1,47 +1,36 @@
 package com.project.ondevicebot;
 
+import android.content.res.AssetFileDescriptor;
+
 import com.project.bff.MainActivity;
 
+import org.python.core.PyDictionary;
+import org.python.core.PyException;
 import org.python.core.PyFile;
 import org.python.core.PyObject;
 import org.tensorflow.lite.Interpreter;
 import org.python.modules.cPickle;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class OnDeviceBotLength1_5
-       extends OnDeviceBotCommon implements OnDeviceBot
+public class OnDeviceBotLength1_5 extends OnDeviceBotCommon
 {
 
-    private static Interpreter tflite;
+    protected static String idx2wordPath = "idx2word1_5.txt";
+    protected static String word2vecPath = "word2vec1_5.txt";
+    protected static String modelPath = "converted_model_v1_1_5.tflite";
+    protected static Interpreter tflite = null;
+    protected static HashMap<Integer, String> idx2word = null;
+    protected static HashMap<String, ArrayList<Float>> word2vec = null;
+    protected MainActivity mainActivity;
 
-    public OnDeviceBotLength1_5()
+    public OnDeviceBotLength1_5(MainActivity _mainActivity)
     {
-        tflite = null;
+        mainActivity = _mainActivity;
     }
 
-    @Override
-    protected String getModelPath()
-    {
-        return new String("converted_model_v1_1_5.tflite");
-    }
-
-    @Override
-    public String generateResponse(String msg, MainActivity mainActivity)
-    {
-        if( tflite == null )
-        {
-            MappedByteBuffer model = loadModelFile(mainActivity);
-            if( model == null )
-            {
-                return ioException;
-            }
-            tflite = new Interpreter(model);
-        }
-
-        cPickle pickler = new cPickle();
-        PyFile file = new PyFile("idx2word1_5.pkl", "r", 1024);
-        return file.toString();
-    }
 }
